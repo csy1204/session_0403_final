290455a2bcf0c3a1f830188f49a9d94982d529cf
csy1204 <josang1204@gmail.com>
2017-04-04 08:28:19 +0000

    final

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  post 'posts' => 'posts#create'
 
+  #resources 'posts'
+  
+  get    '/posts'  => 'posts#index'
+  post  '/posts' =>   'posts#create'
+  
+  get    '/posts/new' =>  'posts#new', as: 'new_post'
+  
+  get    '/posts/:id'   =>   'posts#show', as: 'post'
+  get    '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
+  
+  patch  '/posts/:id'   =>   'posts#update'
+  delete '/posts/:id'   =>   'posts#destroy'
+  # put    '/posts/:id'   =>   'posts#update'
 
 
 
